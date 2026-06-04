9d743f3808b7a3d83b763316006eac7c0202a986
Vasiliy Gladkikh <gladkihvasa275@gmail.com>
2026-06-04 20:02:43 +0300

    небольшой UI для начала, убрал ненужные модули, обновил описание

--- filter-service/src/main/java/ru/kafka/listeners/Listener.java
+++ filter-service/src/main/java/ru/kafka/listeners/Listener.java
@@ -3,6 +3,7 @@ package ru.services.kafka.listeners;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.common.dto.AlertCreatedEvent;
+import ru.services.AlertStreamService;
 import ru.tinkoff.kora.common.Component;
 import ru.tinkoff.kora.kafka.common.annotation.KafkaListener;
 
@@ -11,9 +12,15 @@ public final class Listener {
 
     private static final Logger log = LoggerFactory.getLogger(Listener.class);
 
-    // TODO написать конфиг для кафкить
+    private final AlertStreamService streamService;
+
+    public Listener(AlertStreamService streamService) {
+        this.streamService = streamService;
+    }
+
     @KafkaListener("kafka.alertConsumer")
     public void handle(String key, AlertCreatedEvent event) {
-        log.info("received {}, {}",key, event);
+        log.info("received {}, {}", key, event);
+        streamService.send(event);
     }
 }
